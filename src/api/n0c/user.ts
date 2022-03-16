f4b7cf5cf4c79b02d77cb8b7161bd8778d4b0860
Marc-Andre G <magodin.it@gmail.com>
2022-03-16 14:34:04 -0400

    add user

--- src/api/n0c/user.ts
+++ src/api/n0c/user.ts
@@ -1,14 +1,48 @@
-import { N0cId } from '../../interfaces/n0c.interface';
+import { AddSshKey, N0cId, RemoveSshKey } from '../../interfaces/n0c.interface';
 import { Base } from '../base';
 
 const BASE_PATH = '/n0c-api/user';
 
 export class N0cUser extends Base {
+
   temporaryDomain (params: N0cId) {
-    this.client.sendRequest({
+    return this.client.sendRequest({
       method: 'POST',
       path: `${BASE_PATH}/temp-domain`,
       params
     });
   }
+
+  disableTempDomain (params: N0cId) {
+    return this.client.sendRequest({
+      method: 'POST',
+      path: `${BASE_PATH}/disable-temp-domain`,
+      params
+    });
+  }
+
+  sshKeys (params: N0cId) {
+    return this.client.sendRequest({
+      method: 'GET',
+      path: `${BASE_PATH}/ssh-keys`,
+      params
+    });
+  }
+
+  addSshKey (params: AddSshKey) {
+    return this.client.sendRequest({
+      method: 'POST',
+      path: `${BASE_PATH}/add-ssh-key`,
+      params
+    });
+  }
+
+  editSshKey (params: RemoveSshKey) {
+    return this.client.sendRequest({
+      method: 'POST',
+      path: `${BASE_PATH}/remove-ssh-key`,
+      params
+    });
+  }
+
 }
--- src/interfaces/n0c.interface.ts
+++ src/interfaces/n0c.interface.ts
@@ -2,3 +2,19 @@
 export interface N0cId {
   id: number;
 }
+
+export interface AddSshKey extends N0cId {
+  pKey: string;
+  comment: string;
+  fromIps: string[];
+}
+
+export interface EditSshKey extends N0cId {
+  key: string;
+  comment: string;
+  fromIps: string[];
+}
+
+export interface RemoveSshKey extends N0cId {
+  key: string;
+}
