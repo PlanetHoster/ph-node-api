b4656384fbbb3fd11e9cc27f28a670c2db328df2
Marc-Andre G <magodin.it@gmail.com>
2022-03-17 08:04:38 -0400

    add wp endpoint

--- src/api/n0c/wordpress.ts
+++ src/api/n0c/wordpress.ts
@@ -1,5 +1,12 @@
+import { N0cId } from '../../interfaces/n0c.interface';
 import { Base } from '../base';
 
 export class N0cWordpress extends Base {
-
+  getInstalled (params: N0cId) {
+    return this.client.sendRequest({
+      method: 'GET',
+      path: '/n0c-api/wordpress',
+      params
+    });
+  }
 }
