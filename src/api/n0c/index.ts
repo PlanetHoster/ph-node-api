de1da640e29972e200ef5200a1433c9158d0009f
gandf <gandf@users.noreply.github.com>
2023-05-08 23:48:48 +0200

    Add N0c dns

--- src/api/n0c/index.ts
+++ src/api/n0c/index.ts
@@ -1,5 +1,6 @@
 export * from './user';
 export * from './domain';
+export * from './dns';
 export * from './email';
 export * from './database';
 export * from './cron';
--- src/interfaces/n0c.interface.ts
+++ src/interfaces/n0c.interface.ts
@@ -69,6 +69,16 @@ export interface WafRules extends Domain {
   wafDisabledRules: number[];
   append?: boolean;
 }
+export interface DnsDomain {
+  domain: string;
+  id?: number;
+}
+
+export interface DnsSet {
+  domain: string;
+  id?: number;
+  rrsets: string;
+}
 
 export interface Email extends Domain {
   password: string;
